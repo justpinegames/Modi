99541ffae76ebe4d628e9e2876cccfafb39f8f8b
Vjekoslav <vjekoslav.krajacic@gmail.com>
2012-09-10 19:41:20 +0200

    Added new module that contains HumanClassWritter and MachineClassWritter classes that are responsible for writing as3 classes from Yaml object.

---
+++ src/Modi/ClassWritters.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python
+
+import types
+import re
+
+class HumanClassWritter:
+    file = None
+    package = ""
+    className = ""
+    verbose = False
+
+    def __init__(self, file, package, className, verbose):
+        self.file = file
+        self.package = package
+        self.className = className
+        self.verbose = verbose
+
+    def write(self, text):
+        self.file.write(text)
+        if self.verbose:
+            print text
+
+    def closeFile(self):
+        self.file.close()
+
+    def writeClass(self):
+        if (self.verbose):
+            print "-------------------------------------------------------------------"
+            print "Writing human class: " + self.className + "\n"
+
+        self.write("package " + self.package + "\n{\n\tpublic class " + self.className + " extends _" + self.className + "\n\t{\n")
+        self.write("\n\t\tpublic function " + self.className + "()\n\t\t{\n")
+        self.write("\n\t\t}\n\n\t}\n\n}")
+
+class MachineClassWritter:
+    file = None
+    package = ""
+    className = ""
+    classData = ""
+    verbose = False
+
+    def __init__(self, file, package, className, classData, verbose):
+        self.file = file
+        self.package = package
+        self.className = className
+        self.classData = classData
+        self.verbose = verbose
+
+    def write(self, text):
+        self.file.write(text)
+        if self.verbose:
+            print text
+
+    def closeFile(self):
+        self.file.close()
+
+    def writeClass(self):
+        if self.verbose:
+            print "-------------------------------------------------------------------"
+            print "Writing machine class: _" + self.className + "\n"
+
+        self.writeClassBasics()
+        self.writeAttributeNamesInArray()
+        self.writeAttributeTypes()
+        self.writeAttributeNames()
+        self.writeEnumValues()
+        self.writeAttributes()
+        self.writeConstructor()
+        self.writeGettersAndSetters()
+
+    def writeClassBasics(self):
+        self.write("package " + self.package + "\n{\n")
+        self.write("\timport Modi.*;\n\n")
+        self.write("\timport flash.utils.Dictionary;\n")
+        
+        superClass = "ManagedObject"
+        if "super" in self.classData:
+            superClass = self.classData["super"]
+        self.write("\n\tpublic class _" + self.className + " extends " + superClass + "\n\t{\n")
+
+    def writeAttributeNamesInArray(self):
+        self.write("\t\tpublic static const ATTRIBUTES:Array = [");
+        for attributeName in self.classData:
+            if attributeName != "super":
+                self.write("\"" + attributeName + "\", ")
+        self.write("];\n");
+
+    def writeAttributeTypes(self):
+        self.write("\t\tpublic static const ATTRIBUTE_TYPES:Array = [");
+        for attributeName in self.classData:
+            if attributeName != "super":
+                attributeData = self.classData[attributeName]
+                attributeType = attributeData
+                if "ManagedArray" in attributeData:
+                    attributeType = "ManagedArray"
+                elif type(attributeData) == dict:
+                    if "type" in attributeData:
+                        attributeType = attributeData["type"]
+                    else:
+                        attributeType = "String"
+                self.write("\"" + attributeType + "\", ")
+        self.write("];\n\n")
+
+    def writeAttributeNames(self):
+        for attributeName in self.classData:
+            if attributeName != "super":
+                self.write("\t\tpublic static const ATTRIBUTE_" + toUppercaseWithUnderscores(attributeName) + ":String = \"" + attributeName + "\";\n")
+        self.write("\n")
+
+    def writeEnumValues(self):
+        for attributeName in self.classData:
+            attributeData = self.classData[attributeName]
+            if type(attributeData) == dict and "values" in attributeData:
+                enum = attributeData["values"]
+                enumValues = ""
+                for value in enum:
+                    self.write("\t\tpublic static const " + attributeName.upper() + "_" + str(value).upper() + ":String = \"" + str(value) + "\";\n")
+                    enumValues += "\"" + value + "\", "
+                self.write("\t\tpublic static const " + attributeName.upper() + "_ENUM_ARRAY:Array = [" + enumValues + "];\n\n");
+
+    def writeAttributes(self):
+        for attributeName in self.classData:
+            if attributeName != "super":
+                attributeData = self.classData[attributeName]
+                attributeType = attributeData
+                if "ManagedArray" in attributeType:
+                    attributeType = "ManagedArray"
+                elif type(attributeData) == dict:
+                    if "type" in attributeData:
+                        attributeType = attributeData["type"]
+                    else:
+                        attributeType = "String"
+                self.write("\t\tprivate var _" + attributeName + ":" + attributeType + ";\n")
+
+    def writeConstructor(self):
+        self.write("\n\t\tpublic function _" + self.className + "()\n\t\t{\n")
+        self.write("\t\t\tthis.registerAttributes(ATTRIBUTES, ATTRIBUTE_TYPES);\n")
+        self.write("\n")
+        
+        for attributeName in self.classData:
+            attributeData = self.classData[attributeName]
+            if type(attributeData) == dict:
+                if "default" in attributeData:
+                    if "values" in attributeData:
+                        self.write("\t\t\t_" + attributeName + " = " + attributeName.upper() + "_" + str(attributeData["default"]).upper() + ";\n")
+                    elif "type" in attributeData:
+                        if attributeData["type"] == "String":
+                            self.write("\t\t\t_" + attributeName + " = \"" + str(attributeData["default"]) + "\";\n")
+                        elif attributeData["type"] == "ManagedValue":
+                            self.write("\t\t\t_" + attributeName + " = new ManagedValue(" + str(attributeData["default"]) + ");\n")
+                        else:
+                            self.write("\t\t\t_" + attributeName + " = " + str(attributeData["default"]) + ";\n")
+
+        self.write("\n")
+
+        for attributeName in self.classData:
+            attributeData = self.classData[attributeName]
+            if "ManagedArray" in attributeData and attributeName != "super":
+                self.write("\t\t\t_" + attributeName + " = new ManagedArray();\n")
+                elementType = getManagedArrayElementType(attributeData)
+                if elementType != "ManagedObject":
+                    if isModiClass(elementType):
+                        self.write("\t\t\t_" + attributeName + '.childType = "Modi.' + elementType + '";\n')
+                    else:
+                        packageToAdd = self.package
+                        if self.package != "":
+                            packageToAdd += "."
+                        self.write("\t\t\t_" + attributeName + '.childType = "' + packageToAdd + elementType + '";\n')
+        self.write("\t\t}\n\n")
+
+    def writeGettersAndSetters(self):
+        for attributeName in self.classData:
+            if attributeName != "super":
+                attributeData = self.classData[attributeName]
+                attributeType = attributeData
+                if "ManagedArray" in attributeType:
+                    attributeType = "ManagedArray"
+                elif type(attributeData) == dict:
+                    if "type" in attributeData:
+                        attributeType = attributeData["type"]
+                    else:
+                        attributeType = "String"
+                
+                self.write("\t\tpublic final function set " + attributeName + "(" + attributeName + ":" + attributeType + "):void\n\t\t{\n")
+                self.write("\t\t\tif (!this.allowChange(ATTRIBUTE_" + toUppercaseWithUnderscores(attributeName) + ", _" + attributeName + ", " + attributeName + "))\n")
+                self.write("\t\t\t{\n\t\t\t\treturn;\n\t\t\t}\n\n\t\t\t")
+                self.write("this.willChange(ATTRIBUTE_" + toUppercaseWithUnderscores(attributeName) + ", _" + attributeName + ", " + attributeName + ");\n\n\t\t\t")
+                self.write("var oldValue:" + attributeType + " = _" + attributeName + ";\n\n\t\t\t")
+                self.write("_" + attributeName + " = " + attributeName + ";\n\n\t\t\t")
+                self.write("this.wasChanged(ATTRIBUTE_" + toUppercaseWithUnderscores(attributeName) + ", oldValue, _" + attributeName + ");\n\t\t}\n\n")
+                
+                self.write("\t\tpublic final function get " + attributeName + "():" + attributeType + "\n\t\t{\n\t\t\t")
+                self.write("return _" + attributeName + ";\n\t\t}\n\n")
+                
+                self.write("\t\tpublic final function set " + attributeName.capitalize() + "DirectUnsafe("+ attributeName + ":" + attributeType)
+                self.write("):void\n\t\t{\n\t\t\t_" + attributeName + " = " + attributeName + ";\n\t\t}\n\n")
+        self.write("\t}\n}")
+
+# Converts attribute name to uppercase with underscores between words.
+def toUppercaseWithUnderscores(name):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).upper()
+
+# TODO: Rewrite this function using regular expressions.
+def getManagedArrayElementType(str):
+    childType = "ManagedObject"
+    counts = False
+
+    for char in str:
+        if char == ">":
+            counts = False
+        if counts:
+            childType += char
+        if char == "<":
+            childType = ""
+            counts = True
+    return childType;
+
+# Returns True if className is one of the Modi classes, false otherwise.
+def isModiClass(className):
+    modiClasses = ["ManagedArray", "ManagedValue", "ManagedPoint"]
+    for modiClass in modiClasses:
+        if modiClass == className:
+            return True
+    return False
--- src/Modi/Modi.py
+++ src/Modi/Modi.py
@@ -1,332 +1,116 @@
 #!/usr/bin/python
 
-import sys
-import yaml
 import os
-import types
+import sys
+import getopt
+import yaml
 
-CREATED_FILES = []
-CREATED_FOLDERS = []
+from ClassWritters import HumanClassWritter
+from ClassWritters import MachineClassWritter
+
+VERBOSE = False
 
 def main(argv):
-    
+    # If the user does not specify any argument, call printHelp to show him how to call the script properly.
     if len(argv) == 0:
-        print 'Type "' + sys.argv[0] + ' --help" for instructions on how to call this script properly.'
-    elif len(argv) == 1 and argv[0] == "--help":
         printHelp()
-    else:
-        packageFlag = False
-        outputFlag = False
-        
-        packageDirectory = ""
-        outputDirectory = ""
-        modelFiles = []
 
-        for arg in argv:
-            if packageFlag == True:
-                packageDirectory = arg
-                packageFlag = False
-            elif outputFlag == True:
-                outputDirectory = arg
-                outputFlag = False
-            else:
-                if arg == '-P' or arg == '-p':
-                    packageFlag = True
-                elif arg == '-O' or arg == '-o':
-                    outputFlag = True
-                else:
-                    modelFiles.append(arg)
+    # Retrieve the options and arguments from the user input from the command line.
+    try:
+        opts, args = getopt.gnu_getopt(argv, "m:o:p:hvM:O:P:HV", ["model=", "output=", "package=", "help", "verbose"])
+    except getopt.GetoptError:
+        # If the error occurred while reading the arguments, printhelp will be called and the script will terminate.
+        printHelp()
+        exit()
 
-        if len(modelFiles) == 0:
-            print "Script terminated.\n"
-            print 'Type "Modi.py --help" for instructions on how to call this script properly.'
-        else:
-            packageForCode = packageDirectory
-            packages = packageDirectory.split('.')
-            packageDirectory = ""
-            for package in packages:
-                packageDirectory += package + "/"
-                
-            classesDirectory = os.path.abspath(outputDirectory + "/" + packageDirectory)
-            createDirectory(classesDirectory)
-            
-            
-            for model in modelFiles:
-                generateClassesFromModel(model, classesDirectory, packageForCode)
+    # Any argument that is not preceded by a flag is considered as a model.
+    modelFiles = args
+    package = ""
+    outputDirectory = "."
 
+    # Check for any additional options that the user may have specified.
+    for opt, arg in opts:
+        if opt in ("-h", "-H", "--help"):
+            printHelp()
+        elif opt in ("-o", "-O", "--output"):
+            outputDirectory = arg
+        elif opt in ("-p", "-P", "--package"):
+            package = arg
+        elif opt in ("-v", "-V", "--verbose"):
+            global VERBOSE
+            VERBOSE = True
+
+    packageDirectory = package.replace(".", "/")
+    classesDirectory = os.path.abspath(outputDirectory + "/" + packageDirectory)
+    createDirectory(classesDirectory)
+
+    for model in modelFiles:
+        modelDirectory = os.path.abspath(model)
+        generateClassesFromModel(modelDirectory, classesDirectory, package)
 
 def generateClassesFromModel(model, directory, package):
     try:
         stream = open(model, 'r')
-        
         yamlData = yaml.load_all(stream)
-        
-        for classData in yamlData:
-            for className in classData:
-                createMachineClass(directory, package, className, classData)
-                createHumanClass(directory, package, className, classData)
-                
+
+        for classesData in yamlData:
+            for className in classesData:
+                createMachineClass(directory, package, className, classesData[className])
+                createHumanClass(directory, package, className)
     except yaml.YAMLError, exc:
         if hasattr(exc, 'problem_mark'):
             mark = exc.problem_mark
-            print "Error in YAML file: %s (Line %s, Column %s)" % (path, mark.line + 1, mark.column + 1)
-        cleanAndExit()
+            print "Error in YAML file: %s (Line %s, Column %s)" % (model, mark.line + 1, mark.column + 1)
+        exit()
     except IOError as e:
         print "I/O error({0}): {1}".format(e.errno, e.strerror)
-        cleanAndExit()
-    
+        exit()
 
-def createHumanClass(directory, package, className, classData):
-
-    global CREATED_FILES
+def createHumanClass(directory, package, className):
     classPath = directory + "/" + className + ".as"
     
+    # If the user already generated this human class before, script must NOT override it.
     if not os.path.exists(classPath):
         try:
-            file = open(classPath, "w")
-            """CREATED_FILES.append(classPath)"""
-            try:
-                file.write("package " + package + "\n{\n\tpublic class " + className + " extends _" + className + "\n\t{\n")
-                file.write("\n\t\tpublic function " + className + "()\n\t\t{\n")
-                file.write("\n\t\t}\n\n\t}\n\n}")
-            finally:
-                file.close()
+            humanClassWritter = HumanClassWritter(open(classPath, "w"), package, className, VERBOSE)
+            humanClassWritter.writeClass()
         except IOError:
-            print "Error occoured while opening or reading file: \n" + classPath
-            cleanAndExit()
-
+            print "I/O error({0}): {1}".format(e.errno, e.strerror)
+            exit()
+        finally:
+            humanClassWritter.closeFile()
+            humanClassWritter = None
 
 def createMachineClass(directory, package, className, classData):
-
-    global CREATED_FILES
     classPath = directory + "/_" + className + ".as"    
-    
+
     try:
-        file = open(classPath, "w")
-        CREATED_FILES.append(classPath);
-        
-        try:
-        
-            file.write("package " + package + "\n{\n")
-            file.write("\timport Modi.*;\n\n")
-            file.write("\timport flash.utils.Dictionary;\n")
-            
-            superClass = "ManagedObject"
-            
-            for attributeName in classData[className]:
-                if attributeName == "super":
-                    superClass = classData[className][attributeName]
-            
-            file.write("\n\tpublic class _" + className + " extends " + superClass + "\n\t{\n")
-            
-            """ --------------------------------------------------------------------------- """
-            
-            file.write("\t\tpublic static const ATTRIBUTES:Array = [");
-            for attributeName in classData[className]:
-                if attributeName != "super":
-                    file.write("\"" + attributeName + "\", ")
-            file.write("];\n\t\tpublic static const ATTRIBUTE_TYPES:Array = [");
-            for attributeName in classData[className]:
-                if attributeName != "super":
-                    attributeData = classData[className][attributeName]
-                    attributeType = attributeData
-                    if type(attributeData) == dict:
-                        if "type" in attributeData:
-                            attributeType = attributeData["type"]
-                        else:
-                            attributeType = "String"
-                    if "Managed" in attributeData:
-                        attributeType = getCollectionType(attributeData)
-                    file.write("\"" + attributeType + "\", ")
-            file.write("];\n\n")
-            
-            for attributeName in classData[className]:
-                if attributeName != "super":
-                    file.write("\t\tpublic static const ATTRIBUTE_" + attributeName.upper() + ":String = \"" + attributeName + "\";\n")
-            
-            file.write("\n")
-                
-            for attributeName in classData[className]:
-                attributeData = classData[className][attributeName]
-                if type(attributeData) == dict:
-                    for key in attributeData:
-                        values = attributeData[key]
-                        if type(values) == list:
-                            for value in values:
-                                file.write("\t\tpublic static const " + attributeName.upper() + "_" + str(value).upper() + ":String = \"" + str(value) + "\";\n")
-            
-            for attributeName in classData[className]:
-                attributeData = classData[className][attributeName]
-                if type(attributeData) == dict and "values" in attributeData:
-                    file.write("\t\tpublic static const " + attributeName.upper() + "_ENUM_ARRAY:Array = [");
-                    for key in attributeData:
-                        values = attributeData[key]
-                        if type(values) == list:
-                            for value in values:
-                                file.write("\"" + str(value) + "\", ");
-                    file.write("];\n\n")
-            
-            for attributeName in classData[className]:
-                if attributeName != "super":
-                    attributeData = classData[className][attributeName]
-                    attributeType = ""
-                    if type(attributeData) == dict:
-                        if "values" in attributeData:
-                            attributeType = "String"
-                        elif "type" in attributeData:
-                            attributeType = attributeData["type"]
-                    else:
-                        attributeType = attributeData
-                        if "Managed" in attributeType:
-                            attributeType = getCollectionType(attributeData)
-                    file.write("\t\tprivate var _" + attributeName + ":" + attributeType + ";\n")
-                
-            """ --------------------------------------------------------------------------- """
-            
-            file.write("\n\t\tpublic function _" + className + "()\n\t\t{\n")
-            file.write("\t\t\tthis.registerAttributes(ATTRIBUTES, ATTRIBUTE_TYPES);\n")
-            
-            file.write("\n")
-            
-            for attributeName in classData[className]:
-                attributeData = classData[className][attributeName]
-                if type(attributeData) == dict:
-                    if "default" in attributeData:
-                        if "values" in attributeData:
-                            file.write("\t\t\t_" + attributeName + " = " + attributeName.upper() + "_" + str(attributeData["default"]).upper() + ";\n")
-                        elif "type" in attributeData:
-                            if attributeData["type"] == "String":
-                                file.write("\t\t\t_" + attributeName + ' = "' + str(attributeData["default"]) + '";\n')
-                            else:
-                                file.write("\t\t\t_" + attributeName + " = " + str(attributeData["default"]) + ";\n")
+        machineClassWritter = MachineClassWritter(open(classPath, "w"), package, className, classData, VERBOSE)
+        machineClassWritter.writeClass()
 
-            file.write("\n")
-
-            for attributeName in classData[className]:
-                attributeData = classData[className][attributeName]
-                if "Managed" in attributeData and attributeName != "super":
-                    file.write("\t\t\t_" + attributeName + " = new " + getCollectionType(attributeData) + "();\n")
-
-                    if getChildType(attributeData) != "ManagedObject":
-                        if isModiClass(getChildType(attributeData)):
-                            file.write("\t\t\t_" + attributeName + '.childType = "Modi.' + getChildType(attributeData) + '";\n')
-                        else:
-                            packageToAdd = package
-                            if package != "":
-                                packageToAdd += "."
-
-                            file.write("\t\t\t_" + attributeName + '.childType = "' + packageToAdd + getChildType(attributeData) + '";\n')
-
-            file.write("\t\t}\n\n")
-
-            """ --------------------------------------------------------------------------- """
-            
-            for attributeName in classData[className]:
-                if attributeName != "super":
-                    attributeData = classData[className]
-                    argumentAndReturnType = ""
-                    
-                    if type(attributeData[attributeName]) == dict:
-                        if "values" in attributeData[attributeName]:
-                            argumentAndReturnType = "String"
-                        elif "type" in attributeData[attributeName]:
-                            argumentAndReturnType = attributeData[attributeName]["type"]
-                    else:
-                        argumentAndReturnType = attributeData[attributeName]
-                        if "Managed" in argumentAndReturnType:
-                            argumentAndReturnType = getCollectionType(argumentAndReturnType)
-
-                    file.write("\t\tpublic final function set " + attributeName + "(" + attributeName + ":" + argumentAndReturnType + "):void\n\t\t{\n")
-                    file.write("\t\t\tif (!this.allowChange(ATTRIBUTE_" + attributeName.upper() + ", _" + attributeName + ", " + attributeName + "))\n")
-                    file.write("\t\t\t{\n\t\t\t\treturn;\n\t\t\t}\n\n\t\t\t")
-                    file.write("this.willChange(ATTRIBUTE_" + attributeName.upper() + ", _" + attributeName + ", " + attributeName + ");\n\n\t\t\t")
-                    file.write("var oldValue:" + argumentAndReturnType + " = _" + attributeName + ";\n\n\t\t\t")
-                    file.write("_" + attributeName + " = " + attributeName + ";\n\n\t\t\t")
-                    file.write("this.wasChanged(ATTRIBUTE_" + attributeName.upper() + ", oldValue, _" + attributeName + ");\n\t\t}\n\n")
-                    
-                    file.write("\t\tpublic final function get " + attributeName + "():" + argumentAndReturnType + "\n\t\t{\n\t\t\t")
-                    file.write("return _" + attributeName + ";\n\t\t}\n\n")
-                    
-                    file.write("\t\tpublic final function set " + attributeName.capitalize() + "DirectUnsafe("+ attributeName + ":" + argumentAndReturnType)
-                    file.write("):void\n\t\t{\n\t\t\t_" + attributeName + " = " + attributeName + ";\n\t\t}\n\n")
-            
-            """ --------------------------------------------------------------------------- """
-            
-            file.write("\t}\n}")
-            
-        finally:
-            file.close()
+        # FOR DEBUGGING
+        # machineClassWritter.writeClassBasics()
+        # machineClassWritter.writeAttributeNamesInArray()
+        # machineClassWritter.writeAttributeTypes()
+        # machineClassWritter.writeAttributeNames()
+        # machineClassWritter.writeEnumValues()
+        # machineClassWritter.writeAttributes()
+        # machineClassWritter.writeConstructor()
+        # machineClassWritter.writeGettersAndSetters()
     except IOError:
-        print "Error occoured while opening or reading file: \n" + classPath
-        cleanAndExit()
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        exit()
+    finally:
+        machineClassWritter.closeFile()
+        machineClassWritter = None
 
-def isModiClass(className):
-    modiClasses = ["ManagedArray", "ManagedMap", "ManagedValue", "ManagedPoint"]
-    for modiClass in modiClasses:
-        if modiClass == className:
-            return True
-    
-
-    return False
-
-def isNumeric(value):
-    numericTypes = ["int", "float", "long", "complex"]
-    for numericType in numericTypes:
-        if type(value) == eval(numericType):
-            return True
-    return False
-
-
-def getCollectionType(str):
-    childType = ""
-    counts = True
-
-    for char in str:
-        if char == "<":
-            counts = False
-        if counts:
-            childType += char
-
-    return childType;
-
-def getChildType(str):
-    childType = "ManagedObject"
-    counts = False
-
-    for char in str:
-        if char == ">":
-            counts = False
-        if counts:
-            childType += char
-        if char == "<":
-            childType = ""
-            counts = True
-
-    return childType;
-
-    
-        
-def cleanAndExit():
-    
-    global CREATED_FILES
-    global CREATED_FOLDERS
-    
-    for file in CREATED_FILES:
-        os.remove(file)
-    for folder in CREATED_FOLDERS:
-        os.rmdir(folder)
-    exit()
-    
 def createDirectory(directory):
-    global CREATED_FOLDERS
     if not os.path.exists(directory):
-        os.makedirs(directory)
-        CREATED_FOLDERS.append(directory)
-    
-    
+        os.makedirs(directory)    
+
 def printHelp():
-    print "Ovdje ce doc help manual"
-        
-""" RUN THE SCRIPT WITHOUT FIRST PARAMETER """
+    print "Help."
+
 if __name__ == "__main__":
     main(sys.argv[1:])
+
